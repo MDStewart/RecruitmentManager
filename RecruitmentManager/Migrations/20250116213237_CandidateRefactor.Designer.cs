d689ad6318b1061dd77ae6b79922d7e9dc8d2ccc
Megan D. Stewart <MStewart@mdeq.ms.gov>
2025-01-16 16:26:58 -0600

    Created a Candidate management page

--- RecruitmentManager/Migrations/20250116213237_CandidateRefactor.Designer.cs
+++ RecruitmentManager/Migrations/20250116213237_CandidateRefactor.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using RecruitmentManager.Data;
 
@@ -10,9 +11,11 @@ using RecruitmentManager.Data;
 namespace RecruitmentManager.Migrations
 {
     [DbContext(typeof(HRMSContext))]
-    partial class HRMSContextModelSnapshot : ModelSnapshot
+    [Migration("20250116213237_CandidateRefactor")]
+    partial class CandidateRefactor
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder.HasAnnotation("ProductVersion", "9.0.0");
@@ -24,6 +27,9 @@ namespace RecruitmentManager.Migrations
                         .HasColumnType("INTEGER");
 
                     b.Property<string>("Comment")
+                        .HasColumnType("TEXT");
+
+                    b.Property<DateTime>("DateSubmitted")
                         .HasColumnType("TEXT");
 
                     b.Property<string>("EducationDegree")
---
+++ RecruitmentManager/Models/Applicant.cs
@@ -0,0 +1,35 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace RecruitmentManager.Models;
+
+/// <summary>
+/// Candidate applying to a job posting
+/// </summary>
+public class Applicant
+{
+    [Key]
+    public int Id { get; set; }
+    public string? MiddleName { get; set; }
+
+    public string Address { get; set; }
+
+    public string? Address2 { get; set; }
+
+    public string City { get; set; }
+
+    public string State {  get; set; }
+
+    public string ZipCode { get; set; }
+
+    public string? AlternatePhone { get; set; }
+
+    public DateOnly GraduationDate { get; set; }
+
+    public DateTime? LastModified { get; set; }
+
+    public DateTime DateAdded { get; set; }
+
+    public virtual Job JobPosting {  get; set; }
+
+    public virtual Candidate Candidate { get; set; }
+}
