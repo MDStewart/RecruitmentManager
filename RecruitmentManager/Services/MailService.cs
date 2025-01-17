e3fe2ec26e180f68279c1ba72e3fae2fdce05d0e
Megan D. Stewart <MStewart@mdeq.ms.gov>
2025-01-17 17:00:47 -0600

    Refactored send email functionality

---
+++ RecruitmentManager/Services/MailService.cs
@@ -0,0 +1,16 @@
+﻿using SendGrid.Helpers.Mail;
+using SendGrid;
+
+namespace RecruitmentManager.Services;
+
+public class MailService
+{
+    public async Task SendEmail(EmailAddress from, string subject, EmailAddress to, string plainTextContent, string htmlContent)
+    {
+        string apiKey = Environment.GetEnvironmentVariable("SENDGRID_API_KEY");
+        var client = new SendGridClient(apiKey);
+       
+        var message = MailHelper.CreateSingleEmail(from, to, subject, plainTextContent, htmlContent);
+        var response = await client.SendEmailAsync(message);
+    }
+}
